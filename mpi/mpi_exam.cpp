f66121436f0d9a912caf578af04f92c9c4abe970
David-Hang-12138 <david.hang.12138@gmail.com>
2022-05-06 09:12:40 +0800

    temporary commit

--- mpi/mpi_exam.cpp
+++ mpi/mpi_exam.cpp
@@ -1,8 +1,15 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define MASTER_PROCESS 0
+
 void Usage(char prog_name[]);
+
 int main(int argc, char* argv[]) {
+    int world_size = 0, world_rank = 0;
+
     int A_cnt = 0, B_cnt = 0, C_cnt = 0, D_cnt = 0, F_cnt = 0;
     int Test_Count;
     int i;
@@ -10,12 +17,58 @@ int main(int argc, char* argv[]) {
     /* check and get commad line arguments */
     if (argc != 2) Usage(argv[0]);
     Test_Count = strtol(argv[1], NULL, 10);     //Get number of exams from the commandline
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int rem = Test_Count % world_size;
+    int sum = 0;
+    int *sendcounts = calloc(world_size, sizeof(int));
+    int *displs = calloc(world_size, sizeof(int));
+    int localnum = Test_Count / world_size + 1;
+    int *local_data = calloc(localnum, sizeof(int));
+
+    // calculate send counts and displacements
+    for (int i = 0; i < world_size; i++) {
+        sendcounts[i] = Test_Count/world_size;
+        if (rem > 0) {
+            sendcounts[i]++;
+            rem--;
+        }
+
+        displs[i] = sum;
+        sum += sendcounts[i];
+    }
+
+    // print calculated send counts and displacements for each process
+    if (MASTER_PROCESS == world_rank) {
+        for (int i = 0; i < world_size; i++) {
+            printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, displs[i]);
+        }
+    }
+
     // Allocate space for array data
-    data = malloc(Test_Count * sizeof(int));
+    data = calloc(Test_Count, sizeof(int));
     srandom(time(NULL));   // Seed the random number generator 
  
-    for(i = 0; i < Test_Count; i++)
-        data[i] = 50 + (100 - 50) * random()/((int) RAND_MAX);  // 50 <= data <= 100
+    if (0 == world_rank) {
+        // Generate random data in root process
+        for(i = 0; i < Test_Count; i++)
+            data[i] = 50 + (100 - 50) * random()/((int) RAND_MAX);  // 50 <= data <= 100
+    }
+
+    // divide the data among processes as described by sendcounts and displs
+    MPI_Scatterv(&data, sendcounts, displs, MPI_CHAR, &local_data, localnum, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    printf("Scattered data at rank %d \n", world_rank);
+    for (int i = 0; i < sendcounts[world_rank]; i ++) {
+        if (i == sendcounts[world_rank] - 1) {
+            printf("%d}\n", local_data[i]);
+        } else {
+            printf("%d, ", local_data[i]);
+        }
+    }
 
     // Print list of grades
     printf("\nList of grades:\n");
@@ -41,6 +94,10 @@ int main(int argc, char* argv[]) {
     printf("Number of C = %d\n", C_cnt);
     printf("Number of D = %d\n", D_cnt);
     printf("Number of F = %d\n", F_cnt);
+
+    MPI_Finalize();
+    free(sendcounts);
+    free(displs);
 }
 
 void Usage(char prog_name[]){
